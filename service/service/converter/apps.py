3f822f86ee00723c3297b3636b50a6a3da399e72
Ren Tianxiang <315380189@qq.com>
2025-09-12 11:33:29 +0800

    feat(converter): 读取方法开发

--- service/service/converter/apps.py
+++ service/service/converter/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class ConverterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'converter'
+
+    def ready(self):
+        from django.conf import settings
+        if settings.DEBUG:
+            print("--- 项目已启动，开始执行调试方法 ---")
+            # 导入并调用你想要调试的方法
+            try:
+                from .models import Converter
+                Converter()
+            except Exception as e:
+                print(f"执行调试方法时发生错误: {e}")
+
+            print("--- 调试方法执行完毕 ---")
--- service/service/converter/models.py
+++ service/service/converter/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 import uuid
 from .sketch_parser import load_sketch
+from service.settings import MEDIA_ROOT
+from pathlib import Path
 # Create your models here.
 class Converter(models.Model):
     project_name = models.CharField(max_length=100)
     id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     def __init__(self):
-        self.sketch = load_sketch(r'upload/sketches/555EAECD-44FD-4422-8BAE-E20B3334130D.json')
+        self.sketch = load_sketch(Path (MEDIA_ROOT, 'sketches/555EAECD-44FD-4422-8BAE-E20B3334130D.json'))
         print(self.sketch)
         
 
